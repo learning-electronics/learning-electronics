729b1091b7770c428cc3a7055073b33230d34a8a
AndreClerigo Laptop <andreclerigo@outlook.com>
2022-02-22 01:01:42 +0000

    updated node packages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: '',   redirectTo: '/home', pathMatch: 'full' },
-  { path: '**' , component: PageNotFoundComponent }
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },   /* Default routing to HomeComponent */
+  { path: '**' , component: PageNotFoundComponent }         /* 404 not found will be redirected to PageNotFoundComponent */
 ];
 
 @NgModule({
