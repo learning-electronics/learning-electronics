ac558686af505602a6eb772b6f576e41031687d6
Cláudio <claudioasensio@ua.pt>
2022-03-12 15:57:29 +0000

    Associate username with socketid

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { SharedService, person } from '../shared.service';
 import io from "socket.io-client";
 
 @Component({
@@ -10,8 +11,7 @@ import io from "socket.io-client";
 
 export class GameComponent implements OnInit {
 
-  constructor(private router : Router) { }
-
+  user_info!: person;
   socket: any;
   connections: string[] = [];
   rooms: string[] = [];
@@ -19,20 +19,38 @@ export class GameComponent implements OnInit {
   room_flag: boolean = false;
   socket_id: string = "";
 
+  constructor(private router : Router,private _service: SharedService) {
+    this._service.getAccount().subscribe((data: any) => {
+      if (data.v == true) {
+        this.user_info = data.info as person;
+      }
+    });
+  }
+  
+  
+
   ngOnInit(): void {
     this.socket = io("http://localhost:3000");
     this.rooms = ["None", "Room1", "Room2", "Room3"];
+   
+  }
+
+  ngOnChanges() {
   }
 
   ngAfterViewInit() {
     this.socket.on("connections", (connections : any) => {
       this.connections = connections;
-      console.log(connections); 
+      console.log(connections);
+ 
     });
 
     this.socket.on("socket_id", (id : any) => {
       this.socket_id = id;
+      this.socket.emit("nname",this.user_info?.first_name);
+
     });
+    
   }
 
   ngOnDestroy() {
