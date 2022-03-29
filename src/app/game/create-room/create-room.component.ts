0fcf48a92bdd48c7e86522f6f4415c20a21ebe49
AndreClerigo Laptop <andreclerigo@outlook.com>
2022-03-29 15:29:54 +0100

    added room validation

--- src/app/game/create-room/create-room.component.ts
+++ src/app/game/create-room/create-room.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DialogData } from '../game.component';
 
@@ -8,17 +9,43 @@ import { DialogData } from '../game.component';
   styleUrls: ['./create-room.component.scss']
 })
 export class CreateRoomComponent implements OnInit {
+  form!: FormGroup;
   
-  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData, private dialogRef: MatDialogRef<CreateRoomComponent>) { }
-
-  value : string = "";
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData, private _formBuilder: FormBuilder, private dialogRef: MatDialogRef<CreateRoomComponent>) { }
 
   ngOnInit(): void {
+    this.form = this._formBuilder.group({
+      roomInput: new FormControl("", [Validators.required]),
+    }, {validator: roomValidator}); 
+  }
+
+  /* Shorthands for form controls (used from within template) */
+  get room() { return this.form.get('roomInput'); }
+
+  /* Update validation when the room input changes */
+  onRoomInput() {
+    if (this.form.hasError('nameWrong'))
+      this.room?.setErrors([{'nameWrong': true}]);
+    else
+      this.room?.setErrors(null);
   }
 
   // sends dialog data to game component(name of the room to be created)
   sendNewRoomData() {
-    this.dialogRef.close({name: this.data.name});
+    if (this.form.valid) {
+      this.dialogRef.close({name: this.form.get('roomInput')?.value});
+    }
   }
 
+}
+
+export const roomValidator: ValidatorFn = (formGroup: AbstractControl ): ValidationErrors | null  => {
+  /* Check if the room name already exists */
+  var roomName: string = formGroup.get('roomInput')?.value;
+
+  if (true) {
+    return { nameWrong: true };
+  }
+
+  return null;
 }
