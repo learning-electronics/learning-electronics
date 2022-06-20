1838fc44dddff43f520d2243c30e6a22875c5b25
Cláudio <claudioasensio@ua.pt>
2022-06-20 23:41:56 +0100

    Quizz done (espero)

--- src/app/quizz/show-quizz/show-quizz.component.ts
+++ src/app/quizz/show-quizz/show-quizz.component.ts
@@ -16,68 +16,54 @@ export class ShowQuizzComponent implements OnInit {
   exs:any[]=[];
   studentAnswer: Map<string, string> = new Map<string, string>();
   exsOptions:Map <string,string[]>=new Map<string,string[]>();
-  examData: any;
   nquestions: number = 0;
   deduct: number = 0;
   grade: number = 0;
   questionValue:number=0;
-  first: boolean = true;
-  last: boolean = false;
   currentQuestion:any;
   currentQuestionId:number;
   selectedValue:any;
   end:boolean=false;
-  timeLeft: number=0 ;
+  timeLeft: number=0;
   interval: any;
   minutes: number;
   seconds: any;
-  
+
 
   constructor(private _service: SharedService,private _router:Router) {
     this.subscription = this._service.examStatus.subscribe((data: any) => {
-      //check if data dictionary has exercises key
-
+      //check if test was automatically generated or made by a teacher and retrieve the data
       if (data.exercises!=undefined) {
-        console.log(data);
-        console.log("from class");
         this.exs = data.exercises;
         this.nquestions = data.nquestions;
         this.deduct = data.deduct;
-        this.timeLeft=data.timer* 60;
-        console.log(this.examData);
-        // this.examData=data;
-        // this.exs=this.examData.exercises;
-        // console.log(this.exs);
-        // this.examData.nquestions=this.examData.exercises.length;
-        // console.log(this.examData);
-
-        console.log(data);
+        this.timeLeft=data.timer;
       }else{
-      this.examData=data;
-      console.log(this.examData);
-      this.timeLeft=data.duration*60;  
-      this.getRandomExs(this.nquestions);
-      this.questionValue=20/this.nquestions;
-      // this.currentQuestionId=this.examData.nquestions;
-      console.log(this.exs);
-      console.log(this.questionValue);
+        this.nquestions = data.nquestions;
+        this.timeLeft=data.duration;
+        this.deduct = data.deduct;
+        this.exs=data.exs;  
+        this.getRandomExs(this.nquestions);
       }
-       // this.timeLeft=this.examData.duration*60;
-    this.timeLeft=10*60;  
-    // this.getRandomExs(this.examData.nquestions);
+    this.timeLeft=this.timeLeft*60;  
     this.questionValue=20/this.nquestions;
     
   });
 }
 
-ngOnInit(): void {
-  this.getRandomExs(this.nquestions);
-  this.currentQuestionId=-1;
-  this.getNextQuestion();
-  this.startTimer();
-
+  ngOnInit(): void {
+    this.currentQuestionId=-1;
+    this.getNextQuestion();
+    this.startTimer();
   }
 
+  //stop timer when the component is destroyed
+  ngOnDestroy(): void {
+    
+    clearInterval(this.interval);
+  }
+
+  //shuffle array 
   shuffleArray(array : string[]) {
     for(var i = array.length-1; i > 0; i--) {
       var j = Math.floor(Math.random() * (i+1));
@@ -87,6 +73,7 @@ ngOnInit(): void {
     }
   }
 
+  //if teste is auto generated get random questions from the pool
   getRandomExs(ntimes: number) {
     let exs: any = [];
     for (let i = 0; i < ntimes; i++) {
@@ -97,7 +84,7 @@ ngOnInit(): void {
         this.options.push(this.exs[randomIndex].ans2);
         this.options.push(this.exs[randomIndex].ans3);
         this.options.push(this.exs[randomIndex].correct);
-        this.shuffleArray(this.options);
+        this.shuffleArray(this.options); //shuffle options
         this.exsOptions.set(this.exs[randomIndex].question,this.options);
         this.options = [];
       }else{
@@ -105,7 +92,6 @@ ngOnInit(): void {
       }
     }
     this.exs=exs;
-    console.log(this.exsOptions);
   }
 
   chooseOption(question:string,option: string) {
@@ -127,22 +113,22 @@ ngOnInit(): void {
         this.grade+=this.questionValue;
       }else if ( this.studentAnswer.get(this.exs[i].question)==undefined ) {
         this.grade+=0;
-      
-        this.grade+=0;  
+        this.studentAnswer.set(this.exs[i].question,"Não respondeu");
       } else{
         
         this.grade=this.grade-(this.deduct*this.questionValue);
       }
     }
-    console.log(this.grade);
+    //display grade with onyl 2 decimal cases
+    this.grade=Math.round(this.grade*100)/100;
     this.end=true;
     this.pauseTimer();
-    this.last=false;
-    //navigate to /home after 5 seconds
-    setTimeout(() => {
-      this._router.navigate(['/home']);
-    }, 30000);
+    this.currentQuestionId=0;
   }
+    //go home
+    goHome(){
+      this._router.navigate(['/home']);
+    }
 
    //get 1 question from exs list and remove it from the list
     getNextQuestion(){
@@ -153,27 +139,17 @@ ngOnInit(): void {
       }else{
         this.selectedValue='';
       }
-      // check if is last
-      if(this.currentQuestionId==this.nquestions-1){
-        this.last=true;
-      }
-      
     }
 
     //get previous question
     getPreviousQuestion(){
-
-      //check if is first question
       this.currentQuestionId--;
-      
       this.currentQuestion=this.exs[this.currentQuestionId];
-      
       if (this.studentAnswer.has(this.currentQuestion.question)) {
         this.selectedValue=this.studentAnswer.get(this.currentQuestion.question);
       }else{
         this.selectedValue='';
       }
-     
     }
   
     startTimer() {
